589b9b06a77dafa6cd014831eacb9c13b81b2b7b
StickmanRed <100049599+StickmanRed@users.noreply.github.com>
2024-06-27 15:35:13 -0700

    Update inventory.js

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -1,14 +1,11 @@
 import data from "../data/save.json" with { type: "json" };
 console.log(data);
-$(document).ready(function () {
-    renderInventory()
-});
   
 function returnInventoryContent(name, amount){
     return `<div class="inv-item"><span class="inv-item-name">${name.toUpperCase()} - </span><span class="inv-item-amount">${amount}</span></div>`
 }
 
-function renderInventory(){
+export function renderInventory(){
     data.inventory.forEach((item) => {
         $("#inv-contents").append(returnInventoryContent(item[0], item[1]));
     });
