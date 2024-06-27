75a75deabe7797dbcdd733a0197af0cedcef308f
scannose <73027369+scannose@users.noreply.github.com>
2024-06-27 18:56:33 -0400

    Create combat.js

---
+++ scripts/combat.js
@@ -0,0 +1,19 @@
+const Enemy = class {
+  constructor (name, health, damage) {
+    this.name = name;
+    this.health = health;
+    this.damage = damage;
+  }
+}
+
+// cursed syntax lol
+const Fruit = class extends Enemy {
+  static fruitNames = ["apple", "banana", "orange", "pear"]
+  constructor () {
+    super(
+      fruitNames[Math.floor(Math.random() * fruitNames.length)],
+      Math.floor(Math.random() * 11) + 10,
+      Math.floor(Math.random() * 6) + 5
+    );
+  }
+}
