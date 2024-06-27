8813c924d8986344e2bb0ae7ded42d6dd586df8e
ChekinNooget <aldenyuson@gmail.com>
2024-06-27 16:04:08 -0600

    show inventory (basic)

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -1,2 +1,11 @@
 import data from "../data/save.json" with { type: "json" };
 console.log(data);
+$(document).ready(function () {for (let i = 0; i < data.inventory.length; i++) {
+        document.querySelector("#inv-contents").innerHTML += returnInventoryContent(data.inventory[i][0], data.inventory[i][1])
+    }
+  });
+  
+function returnInventoryContent(name, amount){
+    return `<div class="inv-item"><div class="inv-item-name">${name}</div><div class="inv-item-amount">${amount}</div></div>`
+    
+}
