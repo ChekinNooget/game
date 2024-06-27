30ef08d0d588f5d8f9202302b27784401f8f8148
StickmanRed <100049599+StickmanRed@users.noreply.github.com>
2024-06-27 15:36:30 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+import renderInventory from ../inventory.js
+
 let save = () => localStorage.save = game;
 let load = () => game = localStorage.save;
 
@@ -17,4 +19,5 @@ console.log("1434"); // i lost the game
 
 $(document).ready(function () {
   $("body").append("Script executed.");
+  renderInventory();
 });
