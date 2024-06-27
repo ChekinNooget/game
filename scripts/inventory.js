7c0040439d4db0fc1708f6fca6e4186fb9244d83
StickmanRed <100049599+StickmanRed@users.noreply.github.com>
2024-06-27 15:45:40 -0700

    Update inventory.js

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -5,7 +5,7 @@ function returnInventoryContent(name, amount){
     return `<div class="inv-item"><span class="inv-item-name">${name.toUpperCase()} - </span><span class="inv-item-amount">${amount}</span></div>`
 }
 
-export function renderInventory(){
+export default function renderInventory(){
     data.inventory.forEach((item) => {
         $("#inv-contents").append(returnInventoryContent(item[0], item[1]));
     });
