9f3740b7ee37496d4226d58fcee505654852a1b0
StickmanRed <100049599+StickmanRed@users.noreply.github.com>
2024-06-27 15:59:56 -0700

    Test closures

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,20 @@ import renderInventory from ../inventory.js
 let save = () => localStorage.save = game;
 let load = () => game = localStorage.save;
 
-let game = {time: 0};
+const game = {time: 0};
 
-let lastTick = Date.now();
+/* let lastTick = Date.now();
 let update = () => {
   let curTick = Date.now();
   let delta = curTick - lastTick;
   lastTick = curTick;
   game.time += delta / 1000;
-}
+} */
+
+const update = (() => {
+  let lastTick = Date.now();
+  return () => {let delta = Date.now() - lastTick; lastTick = Date.now(); game.time += delta / 1000;}
+})();
 
 setInterval(update, 100); // every tick is 100 ms
 setInterval(save, 10000); // save every 10 seconds
