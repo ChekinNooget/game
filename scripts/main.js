6694b4d1ddbba9249707e862227095c04ad402a7
aidan0626 <matheasy0626@gmail.com>
2024-06-27 15:49:07 -0700

    awefawef

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-import renderInventory from "./inventory.js";
+import renderInventory from ../inventory.js
 
 let save = () => localStorage.save = game;
 let load = () => game = localStorage.save;
@@ -20,4 +20,5 @@ console.log("1434"); // i lost the game
 $(document).ready(function () {
   $("body").append("Script executed.");
   renderInventory();
+  load();
 });
