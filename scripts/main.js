e0d4df5d24e59f819ca1ce00833bca023ff95701
StickmanRed <100049599+StickmanRed@users.noreply.github.com>
2024-06-27 19:16:13 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
 import renderInventory from "./inventory.js";
 
-let game = {};
+/* Initializes the game */
 // Will "game" be reassigned somewhere?
 // yes, in the update function - it was raising an error
 // This should be coming from something else; const object properties can be reassigned
-// yeah, but it was reassigning the entier variable
+// yeah, but it was reassigning the entire variable
+let game = {};
 let save = () => localStorage.save = game;
 let load = () => {
   try {
@@ -21,28 +22,31 @@ let load = () => {
   }
 };
 
-/* let lastTick = Date.now();
-let update = () => {
-  let curTick = Date.now();
-  let delta = curTick - lastTick;
-  lastTick = curTick;
-  game.time += delta / 1000;
-} */
-
+/* Sets up delta time */
 const update = (() => {
   let lastTick = Date.now();
-  return () => {let delta = Date.now() - lastTick; lastTick = Date.now(); game.time += delta / 1000;}
+  return () => {
+    let delta = Date.now() - lastTick;
+    lastTick = Date.now();
+    game.time += delta / 1000;
+  }
 })();
 
 $(document).ready(function () {
+  /* Setup the inventory */
   renderInventory();
+  /* Starts delta time count */
+  setInterval(update, 100); // every tick is 100 ms (0.1 seconds)
+
+  /* Starts save count */
   load();
-  setInterval(update, 100); // every tick is 100 ms
-  setInterval(save, 10000); // save every 10 seconds
+  setInterval(save, 10000); // saves every 10 seconds
+  
   console.log("1434"); // i lost the game
 });
 
-$("#savebtn").onclick = () => {
+$("#savebtn").click(() => {
   save();
   alert("saved"); // change this later to a popup that doesn't require user interaction
-};
+  // On that note, a popup template would be nice
+});
