8addced004e576256b43fcde20fd815eae3f95ef
StickmanRed <100049599+StickmanRed@users.noreply.github.com>
2024-07-01 14:32:49 -0700

    Create map.js

---
+++ scripts/map.js
@@ -0,0 +1,61 @@
+// Unfinished
+/* The map is a two-dimensional plane
+ * You can travel between any two locations with a straight line drawn between them
+ * Routes have varying costs, some may be inaccessible
+ * Travel time is linearly proportional to distance, calculated by the Pythagorean Theorem
+ * Travel time can be reduced by better equipment or vehicles
+ */
+
+const canvas = $("#map-canvas");
+const ctx = canvas.getContext("2d");
+const RADIUS = 7;
+
+ctx.prototype.circle = function (x, y) {
+  this.beginPath();
+  this.arc(x, y, RADIUS, 0, 2 * Math.PI);
+  this.stroke();
+}
+ctx.prototype.line = function (x1, y1, x2, y2) {
+  this.beginPath();
+  this.moveTo(x1, y1);
+  this.lineTo(x2, y2);
+  this.stroke();
+}
+
+class Map {
+  constructor() {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.nodes = [];
+    this.routes = [];
+  }
+
+  renderRoutes() {
+    this.routes.forEach((route) => {
+      this.ctx.line(route.start.x, route.start.y, route.end.x, route.end.y);
+    });
+  }
+  renderLocations() {
+    this.nodes.forEach((node) => {
+      this.ctx.circle(node.x, node.y);
+    });
+  }
+
+  newNode(x, y, id=Date.now()) {
+    this.nodes.push({
+      id: id.toString(),
+      x: x,
+      y: y
+    });
+  }
+  newRoute(id1, id2, cost, access=false) {
+    this.routes.push({
+      start: id1,
+      end: id2,
+      length: Math.sqrt((id2.x-id1.x)**2 + (id2.y-id1.y)**2),
+      cost: cost,
+      access: access
+    });
+  }
+  
+}
